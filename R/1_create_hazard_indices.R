d615532fe6410d2c78b51c4847b8dacca20345de
Pete Steward <p.steward@cgiar.org>
2023-10-31 11:34:36 +0300

    Updates

--- R/1_create_hazard_indices.R
+++ R/1_create_hazard_indices.R
@@ -5,23 +5,6 @@ require(doFuture)
 
 # NEED TO FIX NEGATIVE VALUES IN PRECIP
 
-# Load metadata for countries to consider in the atlas - exclude islands for which we do not have climate data
-countries_metadata<-fread("Data/metadata/countries.csv")[excluded_island==FALSE]
-
-# What country level data is downloaded locally?
-country_zips<-data.table(filepath=list.files("./Data/country_data_zips",".zip",full.names = T))
-country_zips[,iso3c:=unlist(tstrsplit(tail(tstrsplit(filepath,"/"),1),"-",keep=1)),by=filepath
-][,timeframe:=gsub(".zip","",unlist(tstrsplit(tail(tstrsplit(filepath,"/"),1),"-",keep=2)),"-"),by=filepath
-][timeframe=="seasonal",timeframe:="seasonal_jagermeyer_cc"]
-country_zips[,folder:=gsub(".zip","",unlist(tail(tstrsplit(filepath,"/"),1)))]
-country_zips<-merge(country_zips,countries_metadata[,list(country,iso3)],by.x="iso3c",by.y="iso3")
-
-# Check all countries are represented in the data
-iso3_available<-country_zips[,unique(iso3c)]
-iso3_required<-countries_metadata[,unique(iso3)]
-
-countries_metadata[iso3 %in% iso3_required[!iso3_required %in% iso3_available]]
-
 # Set scenarios and time frames to analyse
 Scenarios<-c("ssp245","ssp585")
 Times<-c("2021_2040","2041_2060")
@@ -131,55 +114,47 @@ PropThreshold<-0.5
 PropTDir=">"
 
 #  Country loop starts here
-country_choice<-"Burundi"
 timeframe_choice<-"annual"
 
-countries<-countries_metadata$country
+# Where are climate data stacks saved?
+clim_dir<-paste0("Data/hazard_timeseries/",timeframe_choice)
+
+# where to save classified climate hazards?
+haz_class_dir<-paste0("Data/hazards_classified/",timeframe_choice)
+if(!dir.exists(haz_class_dir)){
+  dir.create(haz_class_dir,recursive = T)
+}
 
 # Global hazard index savedir
-save_dir_all<-paste0("Data/hazard_indices/",timeframe_choice)
-if(!dir.exists(save_dir_all)){
-  dir.create(save_dir_all)
+haz_index_dir<-paste0("Data/hazard_index/",timeframe_choice)
+if(!dir.exists(haz_index_dir)){
+  dir.create(haz_index_dir,recursive=T)
 }
 
 crop_choices<-c("generic",ms_codes[,sort(Fullname)])
 
-# Create hazard indices for each country x crop x timeperiod x scenario (for the selected timeframe annual/jagermeyer) ####
-# THERE IS AN ISSUE WITH TAI that needs to be debugged - Zambia, Zimbabwe, South Africa are missing
-registerDoFuture()
-plan("multisession", workers = 12)
+# Create classify data for timeperiod x scenario (for the selected timeframe annual/jagermeyer) ####
 
-foreach(i = 1:length(countries)) %dopar% {
-#for(i in 1:length(countries)){
-  country_choice<-countries[i]
-  country_iso3<-countries_metadata[country==country_choice,iso3]
-  country_dir<-paste0("./Data/country_data/", country_zips[country==country_choice & timeframe==timeframe_choice,folder])
-  
-  if(!dir.exists(country_dir)){
-    dir.create(country_dir)
-    unzip(zipfile=country_zips[country==country_choice & timeframe==timeframe_choice,filepath],exdir=country_dir,junkpaths=T)
-  }
-  
-  SaveDir<- paste0(country_dir,"/Analysis")
-  
-  if(!dir.exists(SaveDir)){
-    dir.create(SaveDir)
-  }
-  
-  #  Crop loop starts here
-  #foreach(j = 1:length(crop_choices)) %dopar% { # Hits errors when parallel due to different workers trying to write the same file
-  for(j in 1:length(crop_choices)){ 
+registerDoFuture()
+plan("multisession", workers = 10)
+
+#  Crop loop starts here
+  foreach(j = 1:length(crop_choices)) %dopar% { # Can hit error when parallel due to different workers trying to write the same file
+    # To solve issue loop over unique threshold x variable combinations rather than crops
+    # This will require putting the foreach loop into the hazard wrapper function
+  #for(j in 1:length(crop_choices)){ 
     
     # Display progress
-    cat('\r                                                                                                                     ')
-    cat('\r',paste0("Country ", country_choice," | Crop ",crop_choices[j]))
-    flush.console()
+    print(paste0("Crop ",crop_choices[j]))
+    #cat('\r                                                                                                                     ')
+    #cat('\r',paste0("Crop ",crop_choices[j]))
+    #flush.console()
     
     crop_choice<-crop_choices[j]
-    
-    save_name<-paste0(save_dir_all,"/",country_iso3,"_",crop_choice,"_hi.tif")
+    save_name<-paste0(haz_index_dir,"/hi_",crop_choice,"-",PropThreshold,".tif")
     
     if(!file.exists(save_name)){
+      
       # Create Thresholds Table for crop
       Thresholds<-haz_class[description!="No significant stress" & crop==crop_choice]
       setnames(Thresholds,c("index_name","description","direction"),c("Variable","Severity_class","Direction"))
@@ -190,150 +165,96 @@ foreach(i = 1:length(countries)) %dopar% {
       ][,Code:=paste0(Variable,"_",Code)]
       
       Hazards<-HazardWrapper(Thresholds,
-                            SaveDir=SaveDir,
+                            SaveDir=haz_class_dir,
                             PropThreshold=PropThreshold,
                             PropTDir=PropTDir,
-                            hazard_dir = country_dir,
+                            hazard_dir = clim_dir,
                             Scenarios=Scenarios,
-                            verbose=F)
+                            verbose=T)
       
-      # Hazard Index = severity x recurrence
-      haz_index<-hazard_index(Hazards,
-                              verbose = T,
-                              SaveDir=SaveDir,
-                              crop_choice = crop_choice,
-                              severity_classes=severity_classes,
-                              PropThreshold=PropThreshold)
-      
-      hi_names<-unlist(lapply(1:length(haz_index),FUN=function(k){
-        paste0(names(haz_index)[k],"_",crop_choice,"_",names(haz_index[[k]]))
-      }))
-      
-      haz_index<-terra::rast(haz_index)
-      names(haz_index)<-hi_names
-      
-      writeRaster(haz_index,filename=save_name)
+        # Hazard Index = severity x recurrence
+        haz_index<-hazard_index(Data=Hazards,
+                                hazards=hazards,
+                                verbose = T,
+                                SaveDir=haz_index_dir,
+                                crop_choice = crop_choice,
+                                severity_classes=severity_classes,
+                                PropThreshold=PropThreshold)
+        
+        hi_names<-unlist(lapply(1:length(haz_index),FUN=function(k){
+          paste0(names(haz_index)[k],"_",crop_choice,"_",names(haz_index[[k]]))
+        }))
+        
+        haz_index<-terra::rast(haz_index)
+        names(haz_index)<-hi_names
+        
+        writeRaster(haz_index,filename=save_name)
+      }
     }
     
-  }
-}
 
-# Merge mean hazards across countries ####
-# ISSUE WITH NA values for some variables especially TAI
+# Calculate change for hazard index ####
 
-# Global mean savedir
-save_dir_means<-paste0("Data/hazard_means/",timeframe_choice)
-if(!dir.exists(save_dir_means)){
-  dir.create(save_dir_means,recursive=T)
-}
+files_hi<-paste0("hi_",crop_choices,"-",PropThreshold,".tif")
 
 
-data<-lapply(1:length(countries),FUN=function(j){
-  country_choice<-countries[j]
-  country_dir<-paste0("./Data/country_data/", country_zips[country==country_choice & timeframe==timeframe_choice,folder])
-  SaveDir<- paste0(country_dir,"/Analysis")
-  
-  means<-terra::rast(lapply(1:nrow(Scenarios),FUN=function(i){
-    scenario<-Scenarios[i,combined]
-    files<-list.files(SaveDir,scenario,full.names = T)
-    files<-files[!grepl("hi_",files)]
-    
-    means<-terra::rast(lapply(hazards,FUN=function(hazard){
-      file<-grep(hazard,files,value=T)[1]
-      data<-terra::rast(file)[[1]]
-      data
-    }))
-    
-    names(means)<-paste0(scenario,"_",names(means))
-    names(means)<-gsub("_mean_mean","mean_mean",names(means))
-    names(means)<-gsub("_max_mean","max_mean",names(means))
-    means
-   }))
-  means
+data<-lapply(1:length(crop_choices),FUN=function(j){
+  crop<-crop_choices[j]
+  # create change stack
+  data<-terra::rast(paste0(haz_index_dir,"/hi_",crop,"-",PropThreshold,".tif"))
+  data_hist<-data[[grep("historic",names(data),value=T)]]
 
-})
-data<-terra::sprc(data)
-data<-terra::mosaic(data)
-
-# Fix negative values in precip
-PTOT<-grep("PTOT",names(data),value=T)
-PTOT_layers<-terra::rast(lapply(1:length(PTOT),FUN=function(i){
-  X<-data[[PTOT[i]]]
-  vals<-values(X) 
-  vals[vals<0 & !is.na(vals)]<-NA
-  X[]<-vals
-  X
-}))
-
-data<-c(data[[!grepl("PTOT",names(data))]],PTOT_layers)
-
-terra::writeRaster(data,filename = paste0(save_dir_means,"/haz_means.tif"),overwrite=T)
-
-# create change stack
-data_hist<-data[[grep("historic-historic",names(data))]]
-
-change<-terra::rast(lapply(Scenarios[Scenario!="historic",combined],FUN=function(SCENARIO){
-  data_fut<-data[[grep(SCENARIO,names(data))]]
-  data_fut<-data_fut-data_hist
-  data_fut
-}))
-
-terra::writeRaster(change,filename = paste0(save_dir_means,"/haz_means_change.tif"),overwrite=T)
-
-
-# Merge hazard indices across countries ####
-filenames<-list.files(save_dir_all,full.names = T)
-filenames<-filenames[!grepl("combined_",filenames)]
-for(i in 1:length(crop_choices)){
-  crop<-crop_choices[i]
-  save_name<-paste0(save_dir_all,"/combined_",crop,"_hi.tif")
-  
-  # Display progress
-  cat('\r                                                                                                                     ')
-  cat('\r',paste0("Crop: ",crop))
-  flush.console()
-  
-  if(!file.exists(save_name)){
-    files<-grep(crop,filenames,value = T)
-    
-    data<-terra::sprc(lapply(files,terra::rast))
-    data<-terra::mosaic(data)
-    
-    # Remove delimiter in hazard naming (HSH_max->HSHmax)
-    names(data)<-gsub("_max","max",names(data))
-    names(data)<-gsub("_mean","mean",names(data))
-    
-    terra::writeRaster(data,filename = save_name)
-  }
-  
-}
-
-# calculate change
-for(i in 1:length(crop_choices)){
-  crop<-crop_choices[i]
-  
-  cat('\r                                                                                                                     ')
-  cat('\r',paste0("Crop: ",crop))
-  flush.console()
-  
-  save_name<-paste0(save_dir_all,"/combined_",crop,"_hi.tif")
-  save_name_change<-paste0(save_dir_all,"/combined_",crop,"_hi_change.tif")
-  
-  data<-terra::rast(save_name)
-  data_hist<-data[[grep("historic-historic",names(data))]]
-  
   change<-terra::rast(lapply(Scenarios[Scenario!="historic",combined],FUN=function(SCENARIO){
+        # Display progress
+    cat('\r                                                                                                                     ')
+    cat('\r',paste0("Crop: ",crop," | Scenario:",SCENARIO))
+    flush.console()
+    
+    
     data_fut<-data[[grep(SCENARIO,names(data))]]
-    data_fut-data_hist
+    data_fut<-data_fut-data_hist
+    data_fut
   }))
   
-  terra::writeRaster(change,filename = save_name_change)
+  names(change)<-paste0(names(change),"_change")
   
-}
+  terra::writeRaster(change,filename = paste0(haz_index_dir,"/hi_",crop,"-",PropThreshold,"_change.tif"),overwrite=T)
+  
+})
 
 
-# Combined files
-filenames<-list.files(save_dir_all,"combined_",full.names = T)
+# Calculate change for classified values ####
+files<-list.files(haz_class_dir,".tif",full.names = T)
+files_hist<-grep("historic",files,value = T)
 
-data<-terra::rast(filenames[2])
-plot(data)
+for(i in 1:length(files_hist)){
+  
+  data_hist<-terra::rast(files_hist[i])
+  
+  files_fut<-grep(tstrsplit(files_hist[i],"historic-historic-",keep=2),files,value=T)
+  files_fut<-files_fut[!grepl("historic",files_fut)]
+  
+  for(j in 1:length(files_fut)){
+    data_fut<-terra::rast(files_fut[j])
+    # Display progress
+    cat('\r                                                                                                                     ')
+    cat('\r',paste0("Threshold: ",i," | Scenario: ",j))
+    flush.console()
+    
+    change<-data_fut-data_hist
+    names(change)<-paste0(names(change),"_change")
+    
+    save_name_change<-gsub(".tif","_change.tif",files_fut[j])  
+  
+    terra::writeRaster(change,filename = save_name_change,overwrite=T)
+    
+  }
+ 
+ }
+
+
+files<-list.files(clim_dir,full.names = T)
+files<-grep("PTOT",files,value=T)
+
+PTOT_hist<-rast(files[1])
+PTOT_fut<-rast(files[2])
