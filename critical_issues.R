2f25a98b353c38ed265bb47c6899d8563098ae05
peetmate <p.steward@cgiar.org>
2024-03-22 08:51:28 +0000

    tasks added

--- critical_issues.R
+++ critical_issues.R
@@ -1,7 +1,11 @@
-
+# Hazards ####
 # Update mapspam to USD 2017
 # Update NTx35 and NTx40 to NTxCROP with a crop specific threshold
 # Use crop specific GGCMI Phase 3 crop calendars
 # Integrate moderate, severe and extreme hazards into a single process (64 combinations)
 
-
+# ROI ####
+# Scale mapspam 2005 production to 2017 production?
+# Add human population adoption rates (would also need to integrate with farming system)
+# Add farming systems (I think this would require extraction of value by farming system and administrative area)
+# Herrero data download needs to be added, add data to S3
