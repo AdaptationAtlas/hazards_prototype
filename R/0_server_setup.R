e7c01438ca4f3f62689047781910df7388461c79
peetmate <p.steward@cgiar.org>
2024-06-06 18:02:41 +0000

    dev

--- R/0_server_setup.R
+++ R/0_server_setup.R
@@ -1,4 +1,4 @@
-# 0) Load packages and functions
+# 0) Load packages and functions ####
 
 # Increase download timeout
 options(timeout = 600)
@@ -29,7 +29,7 @@ timeframe_choices<-c("annual","jagermeyr","sos_primary_eos",
                      "sos_primary_fixed_3","sos_primary_fixed_4","sos_primary_fixed_5",
                      "sos_secondary_fixed_3","sos_secondary_fixed_4","sos_secondary_fixed_5")
 
-timeframe_choice<-timeframe_choices[4]
+timeframe_choice<-timeframe_choices[1]
 
 # Increase GDAL cache size
 terra::gdalCache(60000)
@@ -45,7 +45,7 @@ if(!exists("package_dir")){
   package_dir<-getwd()
 }
 
-# 1.1) Where should workflow outputs be stored? #####
+  # 1.1) Where should workflow outputs be stored? #####
 
 # Cglabs
 working_dir<-"/home/jovyan/common_data/hazards_prototype"
@@ -72,260 +72,258 @@ if(timeframe_choice!="annual"){
 }
 
 # 2) Set directories ####
-# 2.1) Local folders #####
-haz_timeseries_dir<-file.path("Data/hazard_timeseries",timeframe_choice)
-if(!dir.exists(haz_timeseries_dir)){dir.create(haz_timeseries_dir,recursive=T)}
-haz_timeseries_s3_dir<-paste0("s3://digital-atlas/risk_prototype/data/hazard_timeseries/",timeframe_choice)
-
-haz_timeseries_monthly_dir<-"Data/hazard_timeseries_mean_month"
-if(!dir.exists(haz_timeseries_monthly_dir)){dir.create(haz_timeseries_monthly_dir,recursive=T)}
-
-haz_time_class_dir<-paste0("Data/hazard_timeseries_class/",timeframe_choice)
-if(!dir.exists(haz_time_class_dir)){dir.create(haz_time_class_dir,recursive=T)}
-
-haz_time_risk_dir<-paste0("Data/hazard_timeseries_risk/",timeframe_choice)
-if(!dir.exists(haz_time_risk_dir)){dir.create(haz_time_risk_dir,recursive=T)}
-
-haz_risk_dir<-paste0("Data/hazard_risk/",timeframe_choice)
-if(!dir.exists(haz_risk_dir)){dir.create(haz_risk_dir,recursive = T)}
-
-haz_mean_dir<-paste0("Data/hazard_timeseries_mean/",timeframe_choice)
-if(!dir.exists(haz_mean_dir)){dir.create(haz_mean_dir,recursive=T)}
-
-haz_sd_dir<-paste0("Data/hazard_timeseries_sd/",timeframe_choice)
-if(!dir.exists(haz_sd_dir)){dir.create(haz_sd_dir,recursive=T)}
-
-haz_time_int_dir<-paste0("Data/hazard_timeseries_int/",timeframe_choice)
-if(!dir.exists(haz_time_int_dir)){dir.create(haz_time_int_dir,recursive=T)}
-
-haz_risk_vop17_dir<-file.path("Data/hazard_risk_vop17",timeframe_choice)
-if(!dir.exists(haz_risk_vop17_dir)){
-  dir.create(haz_risk_vop17_dir,recursive = T)
-}
-
-haz_risk_vop_dir<-file.path("Data/hazard_risk_vop",timeframe_choice)
-if(!dir.exists(haz_risk_vop_dir)){
-  dir.create(haz_risk_vop_dir,recursive = T)
-}
-
-haz_risk_ha_dir<-file.path("Data/hazard_risk_ha",timeframe_choice)
-if(!dir.exists(haz_risk_ha_dir)){
-  dir.create(haz_risk_ha_dir,recursive = T)
-}
-
-haz_risk_n_dir<-file.path("Data/hazard_risk_n",timeframe_choice)
-if(!dir.exists(haz_risk_n_dir)){
-  dir.create(haz_risk_n_dir,recursive = T)
-}
-
-haz_risk_vop_ac_dir<-paste0("Data/hazard_risk_vop_ac/",timeframe_choice)
-if(!dir.exists(haz_risk_vop_ac_dir)){
-  dir.create(haz_risk_vop_ac_dir,recursive=T)
-}
-
-roi_dir<-"Data/roi"
-
-exposure_dir<-"Data/exposure"
-if(!dir.exists(exposure_dir)){
-  dir.create(exposure_dir)
-}
-
-geo_dir<-"Data/boundaries"
-
-if(!dir.exists(geo_dir)){
-  dir.create(geo_dir)
-}
-
-# Inputs
-ac_dir<-"Data/adaptive_capacity"
-if(!dir.exists(ac_dir)){
-  dir.create(ac_dir,recursive=T)
-}
-
-hpop_dir<-"Data/atlas_pop"
-if(!dir.exists(hpop_dir)){
-  dir.create(hpop_dir)
-}
-
-commodity_mask_dir<-"Data/commodity_masks"
-if(!dir.exists(commodity_mask_dir)){
-  dir.create(commodity_mask_dir)
-}
-
-boundary_dir<-"Data/boundaries"
-if(!dir.exists(boundary_dir)){
-  dir.create(boundary_dir)
-}
-
-glw_dir<-"Data/GLW4"
-
-ls_vop_dir<-"Data/livestock_vop"
-
-afr_highlands_dir<-"Data/afr_highlands"
-
-fao_dir<-"Data/fao"
-if(!dir.exists(fao_dir)){
-  dir.create(fao_dir,recursive = T)
-}
-
-mapspam_dir<-"Data/mapspam/2020V1r0_SSA"
-if(!dir.exists(mapspam_dir)){
-  dir.create(mapspam_dir)
-}
-
-# Set sos calendar directory
-sos_dir<-"/home/jovyan/common_data/atlas_sos/seasonal_mean"
-
-
-# 2.2) Atlas s3 bucket #####
-bucket_name <- "http://digital-atlas.s3.amazonaws.com"
-bucket_name_s3<-"s3://digital-atlas"
-s3<-s3fs::S3FileSystem$new(anonymous = T)
+  # 2.1) Local folders #####
+  haz_timeseries_dir<-file.path("Data/hazard_timeseries",timeframe_choice)
+  if(!dir.exists(haz_timeseries_dir)){dir.create(haz_timeseries_dir,recursive=T)}
+  haz_timeseries_s3_dir<-paste0("s3://digital-atlas/risk_prototype/data/hazard_timeseries/",timeframe_choice)
+  
+  haz_timeseries_monthly_dir<-"Data/hazard_timeseries_mean_month"
+  if(!dir.exists(haz_timeseries_monthly_dir)){dir.create(haz_timeseries_monthly_dir,recursive=T)}
+  
+  haz_time_class_dir<-paste0("Data/hazard_timeseries_class/",timeframe_choice)
+  if(!dir.exists(haz_time_class_dir)){dir.create(haz_time_class_dir,recursive=T)}
+  
+  haz_time_risk_dir<-paste0("Data/hazard_timeseries_risk/",timeframe_choice)
+  if(!dir.exists(haz_time_risk_dir)){dir.create(haz_time_risk_dir,recursive=T)}
+  
+  haz_risk_dir<-paste0("Data/hazard_risk/",timeframe_choice)
+  if(!dir.exists(haz_risk_dir)){dir.create(haz_risk_dir,recursive = T)}
+  
+  haz_mean_dir<-paste0("Data/hazard_timeseries_mean/",timeframe_choice)
+  if(!dir.exists(haz_mean_dir)){dir.create(haz_mean_dir,recursive=T)}
+  
+  haz_sd_dir<-paste0("Data/hazard_timeseries_sd/",timeframe_choice)
+  if(!dir.exists(haz_sd_dir)){dir.create(haz_sd_dir,recursive=T)}
+  
+  haz_time_int_dir<-paste0("Data/hazard_timeseries_int/",timeframe_choice)
+  if(!dir.exists(haz_time_int_dir)){dir.create(haz_time_int_dir,recursive=T)}
+  
+  haz_risk_vop17_dir<-file.path("Data/hazard_risk_vop17",timeframe_choice)
+  if(!dir.exists(haz_risk_vop17_dir)){
+    dir.create(haz_risk_vop17_dir,recursive = T)
+  }
+  
+  haz_risk_vop_dir<-file.path("Data/hazard_risk_vop",timeframe_choice)
+  if(!dir.exists(haz_risk_vop_dir)){
+    dir.create(haz_risk_vop_dir,recursive = T)
+  }
+  
+  haz_risk_ha_dir<-file.path("Data/hazard_risk_ha",timeframe_choice)
+  if(!dir.exists(haz_risk_ha_dir)){
+    dir.create(haz_risk_ha_dir,recursive = T)
+  }
+  
+  haz_risk_n_dir<-file.path("Data/hazard_risk_n",timeframe_choice)
+  if(!dir.exists(haz_risk_n_dir)){
+    dir.create(haz_risk_n_dir,recursive = T)
+  }
+  
+  haz_risk_vop_ac_dir<-paste0("Data/hazard_risk_vop_ac/",timeframe_choice)
+  if(!dir.exists(haz_risk_vop_ac_dir)){
+    dir.create(haz_risk_vop_ac_dir,recursive=T)
+  }
+  
+  roi_dir<-"Data/roi"
+  
+  exposure_dir<-"Data/exposure"
+  if(!dir.exists(exposure_dir)){
+    dir.create(exposure_dir)
+  }
+  
+  geo_dir<-"Data/boundaries"
+  
+  if(!dir.exists(geo_dir)){
+    dir.create(geo_dir)
+  }
+  
+  # Inputs
+  ac_dir<-"Data/adaptive_capacity"
+  if(!dir.exists(ac_dir)){
+    dir.create(ac_dir,recursive=T)
+  }
+  
+  hpop_dir<-"Data/atlas_pop"
+  if(!dir.exists(hpop_dir)){
+    dir.create(hpop_dir)
+  }
+  
+  commodity_mask_dir<-"Data/commodity_masks"
+  if(!dir.exists(commodity_mask_dir)){
+    dir.create(commodity_mask_dir)
+  }
+  
+  boundary_dir<-"Data/boundaries"
+  if(!dir.exists(boundary_dir)){
+    dir.create(boundary_dir)
+  }
+  
+  glw_dir<-"Data/GLW4"
+  
+  ls_vop_dir<-"Data/livestock_vop"
+  
+  afr_highlands_dir<-"Data/afr_highlands"
+  
+  fao_dir<-"Data/fao"
+  if(!dir.exists(fao_dir)){
+    dir.create(fao_dir,recursive = T)
+  }
+  
+  mapspam_dir<-"Data/mapspam/2020V1r0_SSA"
+  if(!dir.exists(mapspam_dir)){
+    dir.create(mapspam_dir)
+  }
+  
+  # Set sos calendar directory
+  sos_dir<-"/home/jovyan/common_data/atlas_sos/seasonal_mean"
+  
+  
+  # 2.2) Atlas s3 bucket #####
+  bucket_name <- "http://digital-atlas.s3.amazonaws.com"
+  bucket_name_s3<-"s3://digital-atlas"
+  s3<-s3fs::S3FileSystem$new(anonymous = T)
 # 3) Download key datasets ####
-# 3.1) Geoboundaries #####
-update<-F
-
-geo_files_s3<-c(
-  file.path(bucket_name_s3,"boundaries/atlas-region_admin0_harmonized.parquet"),
-  file.path(bucket_name_s3,"boundaries/atlas-region_admin1_harmonized.parquet"),
-  file.path(bucket_name_s3,"boundaries/atlas-region_admin2_harmonized.parquet"))
-
-geo_files_local<-file.path(geo_dir,basename(geo_files_s3))
-names(geo_files_local)<-c("admin0","admin1","admin2")
-
-lapply(1:length(geo_files_local),FUN=function(i){
-  file<-geo_files_local[i]
-  if(!file.exists(file)|update==T){
-    s3$file_download(geo_files_s3[i],file)
+  # 3.1) Geoboundaries #####
+  update<-F
+  
+  geo_files_s3<-c(
+    file.path(bucket_name_s3,"boundaries/atlas-region_admin0_harmonized.parquet"),
+    file.path(bucket_name_s3,"boundaries/atlas-region_admin1_harmonized.parquet"),
+    file.path(bucket_name_s3,"boundaries/atlas-region_admin2_harmonized.parquet"))
+  
+  geo_files_local<-file.path(geo_dir,basename(geo_files_s3))
+  names(geo_files_local)<-c("admin0","admin1","admin2")
+  
+  lapply(1:length(geo_files_local),FUN=function(i){
+    file<-geo_files_local[i]
+    if(!file.exists(file)|update==T){
+      s3$file_download(geo_files_s3[i],file)
+    }
+  })
+  
+  # 3.2) Mapspam #####
+  update<-T
+  
+  # Specify s3 prefix (folder path)
+  folder_path <- "MapSpam/raw/2020V1r0_SSA/"
+  
+  # List files in the specified S3 bucket and prefix
+  files_s3<-s3fs::s3_dir_ls(file.path(bucket_name_s3,folder_path))
+  files_s3<-files_s3[grepl(".csv",files_s3) & !grepl("index",files_s3)]
+  files_local<-gsub(file.path(bucket_name_s3,folder_path),paste0(mapspam_dir,"/"),files_s3)
+  
+  # If mapspam data does not exist locally download from S3 bucket
+  lapply(1:length(files_local),FUN=function(i){
+    file<-files_local[i]
+    if(!file.exists(file)|update==T){
+      s3$file_download(files_s3[i],file,overwrite=T)
+    }
+  })
+  
+  # 3.3) Base Raster #####
+  # Load base raster to which other datasets are resampled to
+  base_raster<-"base_raster.tif"
+  if(!file.exists(base_raster)){
+    url <- "https://raw.githubusercontent.com/AdaptationAtlas/hazards_prototype/main/metadata/base_raster.tif"
+    httr::GET(url, httr::write_disk(base_raster, overwrite = TRUE))
   }
-})
-
-# 3.2) Mapspam #####
-update<-F
-
-# Specify s3 prefix (folder path)
-folder_path <- "MapSpam/raw/2020V1r0_SSA/"
-
-# List files in the specified S3 bucket and prefix
-files_s3 <- list_s3_bucket_contents(bucket_url=bucket_name, folder_path = folder_path)
-files_s3<-files_s3[grepl(".csv",files_s3) & !grepl("index",files_s3)]
-files_s3<-gsub(bucket_name,bucket_name_s3,files_s3)
-files_local<-gsub(file.path(bucket_name_s3,folder_path),paste0(ls_vop_dir),files_s3)
-
-# If mapspam data does not exist locally download from S3 bucket
-lapply(1:length(files_local),FUN=function(i){
-  file<-files_local[i]
-  if(!file.exists(file)|update==T){
-    s3$file_download(files_s3[i],file)
-  }
-})
-
-# 3.3) Base Raster #####
-# Load base raster to which other datasets are resampled to
-base_raster<-"base_raster.tif"
-if(!file.exists(base_raster)){
-  url <- "https://raw.githubusercontent.com/AdaptationAtlas/hazards_prototype/main/metadata/base_raster.tif"
-  httr::GET(url, httr::write_disk(base_raster, overwrite = TRUE))
-}
-
-base_rast<-terra::rast(base_raster)
-# 3.4) GLW #####
-update<-F
-# If glw data does not exist locally download from S3 bucket
-# Current version is GLW 4
-glw_names<-c(poultry="Ch",sheep="Sh",pigs="Pg",horses="Ho",goats="Gt",ducks="Dk",buffalo="Bf",cattle="Ct")
-glw_codes<-c(poultry=6786792,sheep=6769626,pigs=6769654,horses=6769681,goats=6769696,ducks=6769700,buffalo=6770179,cattle=6769711)
-glw_files <- file.path(glw_dir,paste0("5_",glw_names,"_2015_Da.tif"))
-
-for(i in 1:length(glw_files)){
-  glw_file<-glw_files[i]
-  if(!file.exists(glw_file)|update==T){
-    api_url <- paste0("https://dataverse.harvard.edu/api/access/datafile/",glw_codes[i])
-    # Perform the API request and save the file
-    response <- httr::GET(url = api_url, httr::write_disk(glw_file, overwrite = TRUE))
-    
-    # Check if the download was successful
-    if (httr::status_code(response) == 200) {
-      print(paste0("File ",i," downloaded successfully."))
-    } else {
-      print(paste("Failed to download file ",i,". Status code:", httr::status_code(response)))
+  
+  base_rast<-terra::rast(base_raster)
+  # 3.4) GLW #####
+  update<-F
+  # If glw data does not exist locally download from S3 bucket
+  # Current version is GLW 4
+  glw_names<-c(poultry="Ch",sheep="Sh",pigs="Pg",horses="Ho",goats="Gt",ducks="Dk",buffalo="Bf",cattle="Ct")
+  glw_codes<-c(poultry=6786792,sheep=6769626,pigs=6769654,horses=6769681,goats=6769696,ducks=6769700,buffalo=6770179,cattle=6769711)
+  glw_files <- file.path(glw_dir,paste0("5_",glw_names,"_2015_Da.tif"))
+  
+  for(i in 1:length(glw_files)){
+    glw_file<-glw_files[i]
+    if(!file.exists(glw_file)|update==T){
+      api_url <- paste0("https://dataverse.harvard.edu/api/access/datafile/",glw_codes[i])
+      # Perform the API request and save the file
+      response <- httr::GET(url = api_url, httr::write_disk(glw_file, overwrite = TRUE))
+      
+      # Check if the download was successful
+      if (httr::status_code(response) == 200) {
+        print(paste0("File ",i," downloaded successfully."))
+      } else {
+        print(paste("Failed to download file ",i,". Status code:", httr::status_code(response)))
+      }
     }
   }
-}
-
-# 3.5) Fao stat deflators #####
-update<-F
-# Download FAOstat deflators
-def_file<-paste0(fao_dir,"/Deflators_E_All_Data_(Normalized).csv")
-
-if(!file.exists(def_file)|update==T){
-  # Define the URL and set the save path
-  url<-"https://fenixservices.fao.org/faostat/static/bulkdownloads/Deflators_E_All_Data_(Normalized).zip"
   
-  zip_file_path <- file.path(fao_dir,basename(url))
+  # 3.5) Fao stat deflators #####
+  update<-F
+  # Download FAOstat deflators
+  def_file<-paste0(fao_dir,"/Deflators_E_All_Data_(Normalized).csv")
   
-  # Download the file
-  download.file(url, zip_file_path, mode = "wb")
-  
-  # Unzip the file
-  unzip(zip_file_path, exdir = fao_dir)
-  
-  # Delete the ZIP file
-  unlink(zip_file_path)
-}
-
-# 3.6) Highlands map #####
-update<-F
-
-afr_highlands_file<-file.path(afr_highlands_dir,"afr-highlands.asc")
-
-if(!file.exists(afr_highlands_file)|update==T){
-  download.file(url="https://digital-atlas.s3.amazonaws.com/afr_highlands/afr-highlands.asc",
-                destfile=afr_highlands_file)
-}
-# 3.7) Livestock vop #####
-update<-F
-
-# Specify s3 prefix (folder path)
-folder_path <- "livestock_vop/"
-
-# List files in the specified S3 bucket and prefix
-files_s3 <- list_s3_bucket_contents(bucket_url=bucket_name, folder_path = folder_path)
-files_s3<-files_s3[grepl(".tif",files_s3)]
-files_s3<-gsub(bucket_name,bucket_name_s3,files_s3)
-files_local<-gsub(file.path(bucket_name_s3,folder_path),paste0(ls_vop_dir),files_s3)
-
-# If data does not exist locally download from S3 bucket
-for(i in 1:length(files_local)){
-  file<-files_local[i]
-  if(!file.exists(file)|update==T){
-    s3$file_download(files_s3[i],file)
+  if(!file.exists(def_file)|update==T){
+    # Define the URL and set the save path
+    url<-"https://fenixservices.fao.org/faostat/static/bulkdownloads/Deflators_E_All_Data_(Normalized).zip"
+    
+    zip_file_path <- file.path(fao_dir,basename(url))
+    
+    # Download the file
+    download.file(url, zip_file_path, mode = "wb")
+    
+    # Unzip the file
+    unzip(zip_file_path, exdir = fao_dir)
+    
+    # Delete the ZIP file
+    unlink(zip_file_path)
   }
-}
-
-# 3.8) Human population #####
-# Specify s3 prefix (folder path)
-folder_path <- "population/"
-
-# List files in the specified S3 bucket and prefix
-files_s3 <- list_s3_bucket_contents(bucket_url=bucket_name, folder_path = folder_path)
-files_s3<-files_s3[grepl(".tif",files_s3)]
-files_s3<-gsub(bucket_name,bucket_name_s3,files_s3)
-files_local<-gsub(file.path(bucket_name_s3,folder_path),paste0(hpop_dir),files_s3)
-
-for(i in 1:length(files_local)){
-  file<-files_local[i]
-  if(!file.exists(file)|update==T){
-    s3$file_download(files_s3[i],file)
+  
+  # 3.6) Highlands map #####
+  update<-F
+  
+  afr_highlands_file<-file.path(afr_highlands_dir,"afr-highlands.asc")
+  
+  if(!file.exists(afr_highlands_file)|update==T){
+    download.file(url="https://digital-atlas.s3.amazonaws.com/afr_highlands/afr-highlands.asc",
+                  destfile=afr_highlands_file)
   }
-}
+  # 3.7) Livestock vop #####
+  update<-F
+  
+  # Specify s3 prefix (folder path)
+  folder_path <- "livestock_vop/"
+  
+  # List files in the specified S3 bucket and prefix
+  files_s3<-s3fs::s3_dir_ls(file.path(bucket_name_s3,folder_path))
+  files_s3<-files_s3[grepl(".tif",files_s3)]
+  files_local<-gsub(file.path(bucket_name_s3,folder_path),paste0(ls_vop_dir,"/"),files_s3)
+  
+  # If data does not exist locally download from S3 bucket
+  for(i in 1:length(files_local)){
+    file<-files_local[i]
+    if(!file.exists(file)|update==T){
+      s3$file_download(files_s3[i],file)
+    }
+  }
+  
+  # 3.8) Human population #####
+  # Specify s3 prefix (folder path)
+  folder_path <- "population/worldpop_2020/"
+  
+  # List files in the specified S3 bucket and prefix
+  files_s3<-s3fs::s3_dir_ls(file.path(bucket_name_s3,folder_path))
+  files_s3<-files_s3[grepl("pop.tif",files_s3)]
+  files_local<-gsub(file.path(bucket_name_s3,folder_path),paste0(hpop_dir,"/"),files_s3)
+  
+  for(i in 1:length(files_local)){
+    file<-files_local[i]
+    if(!file.exists(file)|update==T){
+      s3$file_download(files_s3[i],file)
+    }
+  }
 
 # 4) Set data paths ####
-# 4.1) hazard class #####
-haz_class_url<-"https://raw.githubusercontent.com/AdaptationAtlas/hazards_prototype/main/metadata/haz_classes.csv"
-# 4.2) hazard metadata #####
-haz_meta_url<-"https://raw.githubusercontent.com/AdaptationAtlas/hazards_prototype/main/metadata/haz_metadata.csv"
-# 4.3) mapspam codes #####
-ms_codes_url<-"https://raw.githubusercontent.com/AdaptationAtlas/hazards_prototype/main/metadata/SpamCodes.csv"
-# 4.4) ecocrop ####
-ecocrop_url<-"https://raw.githubusercontent.com/AdaptationAtlas/hazards_prototype/main/metadata/ecocrop.csv"
+  # 4.1) hazard class #####
+  haz_class_url<-"https://raw.githubusercontent.com/AdaptationAtlas/hazards_prototype/main/metadata/haz_classes.csv"
+  # 4.2) hazard metadata #####
+  haz_meta_url<-"https://raw.githubusercontent.com/AdaptationAtlas/hazards_prototype/main/metadata/haz_metadata.csv"
+  # 4.3) mapspam codes #####
+  ms_codes_url<-"https://raw.githubusercontent.com/AdaptationAtlas/hazards_prototype/main/metadata/SpamCodes.csv"
+  # 4.4) ecocrop ####
+  ecocrop_url<-"https://raw.githubusercontent.com/AdaptationAtlas/hazards_prototype/main/metadata/ecocrop.csv"
+  
--- R/2.2_ptot_area_change.R
+++ R/2.2_ptot_area_change.R
@@ -26,11 +26,63 @@ packages <- c("arrow",
 # Call the function to install and load packages
 load_and_install_packages(packages)
 
+merge_admin_extract<-function(data_ex) {
+  
+  # Define a mapping of administrative level names to short codes.
+  levels <- c(admin0 = "adm0", admin1 = "adm1", admin2 = "adm2")
+  
+  # Create filenames for saving the output based on administrative level and aggregation function.
+  file0 <- gsub("_adm_", "_adm0_", file)
+  file1 <- gsub("_adm_", "_adm1_", file)
+  file2 <- gsub("_adm_", "_adm2_", file)
+  
+  
+  # Process the extracted data to format it for analysis or further processing.
+  data_ex <- rbindlist(lapply(1:length(levels), FUN = function(i) {
+    level <- levels[i]
+    
+    # Convert the data to a data.table and remove specific columns.
+    data <- data.table(data.frame(data_ex[[names(level)]]))
+    data <- data[, !c("admin_name", "iso3")]
+    
+    # Determine the administrative level being processed and adjust the data accordingly.
+    admin <- "admin0_name"
+    if (level %in% c("adm1", "adm2")) {
+      admin <- c(admin, "admin1_name")
+      data <- suppressWarnings(data[, !"a1_a0"])
+    }
+    
+    if (level == "adm2") {
+      admin <- c(admin, "admin2_name")
+      data <- suppressWarnings(data[, !"a2_a1_a0"])
+    }
+    
+    # Adjust column names and reshape the data.
+    colnames(data) <- gsub("_nam$", "_name", colnames(data))
+    data <- data.table(melt(data, id.vars = admin))
+    
+    data
+  }), fill = T)
+  
+  
+  # Return the processed or read data.
+  return(data_ex)
+}
+
 # 0.1) Set up workspace #####
 haz_class<-fread(haz_class_url)
 
 # Make cell size raster
 base_cellsize<-terra::cellSize(base_rast,unit="km")
+
+# Load admin boundaries
+Geographies<-lapply(1:length(geo_files_local),FUN=function(i){
+  file<-geo_files_local[i]
+  data<-arrow::open_dataset(file)
+  data <- data |> sf::st_as_sf() |> terra::vect()
+  data
+})
+names(Geographies)<-names(geo_files_local)
 
 # 1) % area of precipitation increase or decrease by admin vect ####
 # Create save folder
@@ -94,49 +146,6 @@ change_dec<-admin_extract(change_dec,
                           FUN = "sum")
 
 # Tabulate data
-merge_admin_extract<-function(data_ex) {
-  
-  # Define a mapping of administrative level names to short codes.
-  levels <- c(admin0 = "adm0", admin1 = "adm1", admin2 = "adm2")
-  
-  # Create filenames for saving the output based on administrative level and aggregation function.
-  file0 <- gsub("_adm_", "_adm0_", file)
-  file1 <- gsub("_adm_", "_adm1_", file)
-  file2 <- gsub("_adm_", "_adm2_", file)
-  
-  
-  # Process the extracted data to format it for analysis or further processing.
-  data_ex <- rbindlist(lapply(1:length(levels), FUN = function(i) {
-    level <- levels[i]
-    
-    # Convert the data to a data.table and remove specific columns.
-    data <- data.table(data.frame(data_ex[[names(level)]]))
-    data <- data[, !c("admin_name", "iso3")]
-    
-    # Determine the administrative level being processed and adjust the data accordingly.
-    admin <- "admin0_name"
-    if (level %in% c("adm1", "adm2")) {
-      admin <- c(admin, "admin1_name")
-      data <- suppressWarnings(data[, !"a1_a0"])
-    }
-    
-    if (level == "adm2") {
-      admin <- c(admin, "admin2_name")
-      data <- suppressWarnings(data[, !"a2_a1_a0"])
-    }
-    
-    # Adjust column names and reshape the data.
-    colnames(data) <- gsub("_nam$", "_name", colnames(data))
-    data <- data.table(melt(data, id.vars = admin))
-    
-    data
-  }), fill = T)
-  
-  
-  # Return the processed or read data.
-  return(data_ex)
-}
-
 change_inc<-merge_admin_extract(change_inc)[,direction:="increase_5"]
 change_dec<-merge_admin_extract(change_dec)[,direction:="decrease_5"]
 base_areas<-merge_admin_extract(base_areas)[,direction:="total"]
@@ -161,6 +170,7 @@ change_ens<-change[!grepl("ENSEMBLE",model)]
 change_ens<-change_ens[,list(mean=mean(value,na.rm=T),min=min(value,na.rm=T),max=max(value,na.rm=T),sd=sd(value,na.rm=T)),
                        by=list(admin0_name,admin1_name,admin2_name,scenario,timeframe,direction,variable,stat)]
 
+# save results
 arrow::write_parquet(change,file.path(haz_mean_ptot_dir,"ptot_change_by_model.parquet"))
 arrow::write_parquet(change_ens,file.path(haz_mean_ptot_dir,"ptot_change_ensemble.parquet"))
 
@@ -173,15 +183,13 @@ if(!dir.exists(haz_mean_thi_dir)){
 }
 
 # list data files
-files<-list.files(haz_time_risk_dir,full.names =T)
-files<-grep("THI_max",files,value=T)
+files<-list.files(haz_time_risk_dir,"THI_max",full.names =T)
 
 # get severity thresholds
 cat_thresholds<-haz_class[index_name=="THI_max" & 
                             description %in% c("Severe","Extreme") & 
                             crop  %in% c("cattle_highland","cattle_tropical"),list(index_name,crop,description,threshold)
-                          ][,code:=paste0("THI_max_max-G",threshold)
-                            ][,code2:=paste(c(unlist(tstrsplit(crop,"_",keep=2)),tolower(description)),collapse = "_"),by=code]
+                          ][,code:=paste0("THI_max_max-G",threshold)]
 
 # get highland/lowland mask
 highlands<-terra::rast(afr_highlands_file)
@@ -189,23 +197,66 @@ highlands<-terra::resample(highlands,base_rast,method="near")
 tropical<-classify(highlands,data.frame(from=c(0,1),to=c(1,0)))
 
 # subset data files to 
-i<-1
-
-data<-lapply(1:nrow(cat_thresholds),FUN=function(i){
+data<-pblapply(1:nrow(cat_thresholds),FUN=function(i){
   files_ss<-grep(cat_thresholds[i,code],files,value=T)
   data<-terra::rast(files_ss)
-  names(data)<-paste0(gsub(".tif","",basename(files_ss)),"_",cat_thresholds[i,code2])
+  
+  # Apply highland/lowland mask
   if(cat_thresholds[i,grepl("tropical",crop)]){
-    data<-data*cat_mask_tropical
+    data<-data*tropical
   }else{
-    
+    data<-data*highlands
   }
+  
+  names(data)<-paste0(gsub(".tif","",basename(files_ss)),"_",cat_thresholds[i,tolower(description)])
   
   data
 })
 
-data_trop<-grep("tropical",cat_thresholds$crop)
+data_sev<-data[grep("Severe",cat_thresholds$description)]
+data_sev<-data_sev[[1]]+data_sev[[2]]
 
+data_ext<-data[grep("Extreme",cat_thresholds$description)]
+data_ext<-data_ext[[1]]+data_ext[[2]]
 
-data<-base_cellsize*data
+data<-c(data_sev,data_ext)
+data<-data*base_cellsize
 
+# Extract by admin area
+base_areas<-admin_extract(base_cellsize,
+                          Geographies = Geographies,
+                          FUN = "sum")
+
+data<-admin_extract(data,
+                    Geographies = Geographies,
+                    FUN = "sum")
+
+# Tabulate data
+data<-merge_admin_extract(data)
+base_areas<-merge_admin_extract(base_areas)
+setnames(base_areas,"value","total")
+
+# Work out percentage change
+data<-merge(data,base_areas[,list(admin0_name,admin1_name,admin2_name,total)],all.x=T)
+data[,value:=round(100*value/total,1)][,total:=NULL]
+
+# Wrangle variable name
+var_names<-data$variable
+var_names<-gsub("sum.|_THI_max_max","",var_names)
+var_names<-gsub("1_2","1-2",var_names)
+var_names<-gsub(".G","_",var_names)
+var_names<-gsub("historical","historical_historical_historical",var_names)
+var_names<-do.call("cbind",tstrsplit(var_names,"_"))[,c(1:3,5)]
+colnames(var_names)<-c("scenario","model","timeframe","severity")
+
+data<-cbind(data,var_names)[,variable:="THI"][,stat:="perc_area"][,crop:="cattle"]
+
+# Generate ensemble data from models
+data_ens<-data[,list(mean=mean(value,na.rm=T),
+                     min=min(value,na.rm=T),
+                     max=max(value,na.rm=T),
+                     sd=sd(value,na.rm=T)),
+                       by=list(admin0_name,admin1_name,admin2_name,scenario,timeframe,variable,severity,stat,crop)]
+
+arrow::write_parquet(data,file.path(haz_mean_thi_dir,"thi_perc_area_by_model.parquet"))
+arrow::write_parquet(data_ens,file.path(haz_mean_thi_dir,"thi_perc_area_ensemble.parquet"))
