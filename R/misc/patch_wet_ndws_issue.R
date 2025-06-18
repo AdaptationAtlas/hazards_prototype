ec6c5c3409acbd92c92a713fe7fe222634023952
peetmate <p.steward@cgiar.org>
2025-06-18 08:01:34 +0000

    patch updated to run mutliple dirs and use parallel

--- R/misc/patch_wet_ndws_issue.R
+++ R/misc/patch_wet_ndws_issue.R
@@ -5,9 +5,9 @@ patch_dirs<-c(atlas_dirs$data_dir$hazard_risk,
               atlas_dirs$data_dir$hazard_risk_vop_usd)
 
 for(i in length(patch_dirs)){
-  cat("Patching ",patch_dirs[j])
+  cat("Patching ",patch_dirs[i],"\n")
   
-  dir1<-file.path(patch_dirs[j],timeframe_choices)
+  dir1<-file.path(patch_dirs[i],timeframe_choices)
   
   tifs<-list.files(dir1,"tif",full.names=T)
   tifs<-tifs[!grepl("[+]",tifs)]
@@ -16,31 +16,69 @@ for(i in length(patch_dirs)){
   parquets<-list.files(dir1,"solo.*parquet",full.names=T)
   parquets<-parquets[!grepl("json",parquets)]
   
-  for(j in 1:length(tifs)){
-    cat(j,"/",length(tids),"       \r")
-    file<-tifs[j]
-    rdat<-rast(file)
-    if(any(grepl("wet_NDWS",names(rdat)))){
-    names(rdat)<-gsub("wet_NDWS","dry_NDWS",names(rdat))
-    rdat<-rdat+0
-    terra::writeRaster(rdat,
-                       file=file,
-                       overwrite=T,
-                       filetype = 'COG',
-                       gdal = c("COMPRESS=ZSTD"))     
-    }
-  }
+  cat("Patching ",patch_dirs[i],"- tifs \n")
   
-  for(j in 1:length(parquets)){
-    cat(j,"/",length(parquets),"       \r")
-    file<-parquets[j]
-    dat<-read_parquet(file)
-    dat<-data.table(dat)
-    if(dat[hazard=="wet" & hazard_vars=="NDWS",.N>0]){
-      dat[hazard=="wet" & hazard_vars=="NDWS",hazard:="dry"]
-      write_parquet(dat,file)     
-    }
-  }
+  set_parallel_plan(n_cores=10,use_multisession=T)
   
-  cat("Patching complete to fix wet ndws issue")
-  }
+  # Enable progressr
+  progressr::handlers(global = TRUE)
+  progressr::handlers("progress")
+  
+  # Wrap the parallel processing in a with_progress call
+  p<-with_progress({
+    # Define the progress bar
+    prog <- progressr::progressor(along = 1:length(tifs))
+    
+    invisible(
+      future.apply::future_lapply(1:length(tifs),FUN=function(j){
+        #    cat(j,"/",length(tids),"       \r")
+        file<-tifs[j]
+        rdat<-rast(file)
+        if(any(grepl("wet_NDWS",names(rdat)))){
+        names(rdat)<-gsub("wet_NDWS","dry_NDWS",names(rdat))
+        rdat<-rdat+0
+        terra::writeRaster(rdat,
+                           file=file,
+                           overwrite=T,
+                           filetype = 'COG',
+                           gdal = c("COMPRESS=ZSTD"))     
+        }
+   })
+   )
+  
+  })
+  
+  plan(sequential)
+  
+  cat("Patching ",patch_dirs[j],"- parquets \n")
+  set_parallel_plan(n_cores=10,use_multisession=T)
+  
+  # Enable progressr
+  progressr::handlers(global = TRUE)
+  progressr::handlers("progress")
+  
+  # Wrap the parallel processing in a with_progress call
+  p<-with_progress({
+    # Define the progress bar
+    prog <- progressr::progressor(along = 1:length(parquets))
+    
+    invisible(
+      future.apply::future_lapply(1:length(parquets),FUN=function(j){
+      #  cat(j,"/",length(parquets),"       \r")
+        file<-parquets[j]
+        dat<-read_parquet(file)
+        dat<-data.table(dat)
+        if(dat[hazard=="wet" & hazard_vars=="NDWS",.N>0]){
+          dat[hazard=="wet" & hazard_vars=="NDWS",hazard:="dry"]
+          write_parquet(dat,file)     
+        }
+      })
+    )
+  })
+  
+  plan(sequential)
+  
+  cat("Patching",patch_dirs[i]," complete\n")
+}
+
+cat("All patching complete\n")
