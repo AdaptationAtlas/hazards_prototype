6e50787c582f98fd72a3a024f0a99fbc4e28da62
peetmate <p.steward@cgiar.org>
2025-06-27 17:33:11 +0000

    kicking ass

--- R/misc/haz_exposure_worked_example.R
+++ R/misc/haz_exposure_worked_example.R
@@ -1,6 +1,6 @@
 
-pacman::p_load(terra,duckdb,geoarrow,arrow,sf)
-
+pacman::p_load(terra,duckdb,geoarrow,arrow,sf,ggupset,ggplot2)
+options(scipen = 999)
 
 # Set choice/meta-data vectors ####
 
@@ -128,7 +128,7 @@ setnames(exp_tot,"value","value_tot")
 
 
 
-# Access Hazard exposure ####
+  # Access Hazard exposure ####
 s3_parquet_paths<-data.table(expand.grid(model=models,
                                          severity=severities,
                                          variable=variables,
@@ -330,13 +330,18 @@ if(nrow(haz_s1)!=nrow(haz_s2)){
   haz_tot_geo <- haz_merge[, lapply(.SD, sum, na.rm = TRUE), by = agg_cols, .SDcols = num_cols]
   haz_tot_geo<-haz_tot_geo[,crop:="all"]
   
+  # Grand total across all geo
+  agg_cols <- c("severity","scenario1","scenario2","crop","hazard")
+  haz_tot_crop <- haz_merge[, lapply(.SD, sum, na.rm = TRUE), by = agg_cols, .SDcols = num_cols]
+  haz_tot_crop[,c("admin0_name","admin1_name","admin2_name"):="all"]
+  
   # Bind back grouped data
-  haz_merge<-rbind(haz_merge,haz_tot,haz_tot_geo)
+  haz_merge<-rbindlist(list(haz_merge,haz_tot,haz_tot_geo,haz_tot_crop),use.names=T)
   
   # Calculate difference ####
   haz_merge[,diff:=value2-value1]
   
-  # Calulate % exposure to hazards ####
+  # Calculate relative exposure & errors ####
   ci_95 <- function(mean, sd, n) {
     error <- qt(0.975, df = n - 1) * sd / sqrt(n)
     lower <- mean - error
@@ -361,15 +366,333 @@ if(nrow(haz_s1)!=nrow(haz_s2)){
   haz_merge[,c("value2_low","value2_high"):=ci_95(value2,value_sd2,5),by=.I]
   haz_merge[,c("perc2_low","perc2_high"):=round(ci_95(perc2,perc_sd2,5),2),by=.I]
   
-  # Rename heat, dry, wet to heat_only etc.
+  # Rename hazard labels and create hazard sets ####
   haz_merge[hazard=="heat",hazard:="heat (only)"]
   haz_merge[hazard=="wet",hazard:="wet (only)"]
   haz_merge[hazard=="dry",hazard:="dry (only)"]
+  haz_merge[hazard=="any",hazard:="any hazard"]
+  
+  # Define haz sets
+  compound_set_full<-c("no hazard","dry (only)","heat (only)","wet (only)","dry+heat","dry+wet","heat+wet","dry+heat+wet")
+  compound_set_simple<-c("no hazard","1 hazard","2 hazards","3 hazards")
+  solo_set<-c("no hazard","dry (any)","heat (any)","wet (any)","any")
+  
+  # Create palettes
+  # Build a pallete 
+  mix_hex <- function(col1, col2, p = 0.5) {
+    stopifnot(is.numeric(p), p >= 0, p <= 1)
+    rgb <- grDevices::colorRamp(c(col1, col2), space = "Lab")(p)   # 0–255
+    grDevices::rgb(rgb[1], rgb[2], rgb[3], maxColorValue = 255)    # back to hex
+  }
+  
+  #Pick three base hues for the single hazards
+  palette<-"Set 2"
+  base_hues <- qualitative_hcl(3, palette = palette)   # any palette is fine
+  names(base_hues) <- c("dry", "heat", "wet")
+  
+  # Build a named vector of final fill colours
+  cols_fill <- c(
+    ## 0-hazard (grey)
+    "no hazard"      = "grey85",
+    
+    ## 1-hazard (very light versions of the bases)
+    "dry (only)"     = as.character(lighten(base_hues["dry"],  .5)),
+    "heat (only)"    = as.character(lighten(base_hues["heat"], .5)),
+    "wet (only)"     = as.character(lighten(base_hues["wet"],  .5)),
+    
+    ## 2-hazard (medium lightness – mix two bases, then lighten a bit)
+    "dry+heat"       = lighten(mix_hex(base_hues["dry"],  base_hues["heat"], .50), -0.1),
+    "dry+wet"        = lighten(mix_hex(base_hues["dry"],  base_hues["wet"],  .50), -0.1),
+    "heat+wet"       = lighten(mix_hex(base_hues["heat"], base_hues["wet"],  .50), -0.1),
+    
+    ## 3-hazard (darkest – mix the three bases 1/3-1/3-1/3)
+    "dry+heat+wet"   = lighten(mix_hex(
+      mix_hex(base_hues["dry"], base_hues["heat"], .50),
+      base_hues["wet"], .67),-0.5)
+  )
+  
+  cols_fill_simple <- c(
+    "no hazard"      = "grey85",
+    "1 hazard"     = as.character(lighten(base_hues["dry"],  .7)),
+    "2 hazards"     = as.character(lighten(base_hues["dry"],  .35)),
+    "3 hazards"     = as.character(base_hues["dry"])
+  )
+  
+  cols_fill_solo <- c(
+    "no hazard"      = "grey85",
+    "heat (any)"     = as.character(base_hues["heat"]),
+    "dry (any)"     = as.character(base_hues["dry"]),
+    "wet (any)"     = as.character(base_hues["wet"]),
+    "any"           = lighten(mix_hex(
+      mix_hex(base_hues["dry"], base_hues["heat"], .50),
+      base_hues["wet"], .67),-0.5)
+  )
   
   # Plots ####
-  # Q1: Upset plot
+  # Q1: Upset plot ####
   
-  # Q2: Crops>Hazards
+  # Bonus points if error_bars can be added
+  q1_dat<-data[admin0_name=="all" & hazard %in% compound_set_full]
+
+  
+  up_plot<-function(data,val_col,exp_lab,plot_title,val_alt=NULL){
+  
+  q1_dat<-data
+  
+  q1_dat[, dry  := grepl("\\bdry\\b",  hazard)]
+  q1_dat[, heat := grepl("\\bheat\\b", hazard)]
+  q1_dat[, wet  := grepl("\\bwet\\b",  hazard)]
+
+  # Convert logicals to hazard list column
+  q1_dat[, hazards := apply(.SD, 1, function(row) names(row)[as.logical(row)]), .SDcols = c("dry", "heat", "wet")]
+
+  setnames(q1_dat,val_col,"value")
+  
+  if(!is.null(val_alt)){
+    q1_dat[,valx:=value]
+  }else{
+    q1_dat[,valx:=val_alt,with=F]
+  }
+  
+  if(q1_dat[,max(valx)>10^9]){
+    q1_dat[,value:=value/10^9]
+    lab<-paste0("Exposure B ",exp_lab)
+    }else{
+  if(q1_dat[,max(valx)>10^6]){
+    q1_dat[,value:=value/10^6]
+    lab<-paste0("Exposure M ",exp_lab)
+  }else{
+    if(q1_dat[,max(valx)>10^3]){
+      q1_dat[,value:=value/10^3]
+      lab<-paste0("Exposure k ",exp_lab)
+    }else{
+      lab<-paste0("Exposure ",exp_lab)
+    }}}
+  
+  # Use ggupset to make upset ploy
+  p_up<-ggplot(q1_dat, aes(x = hazards, y = value)) +
+    geom_col(fill = "grey40") +
+    geom_text(aes(label = scales::comma(value)), vjust = -0.3, size = 3) +
+    scale_x_upset(order_by = NULL) +
+    labs(
+      title = plot_title,
+      x = NULL,
+      y = lab
+    ) +
+    theme_minimal()+
+    theme(
+      axis.text.x        = element_blank(),
+      axis.ticks.x       = element_blank(),
+      panel.grid = element_blank(),
+      plot.margin        = margin(t = 0, r = 0, b = 0, l = 0)  
+    )
+  
+  # Make bars for total hazard exposure
+  haz_cols <- c("dry","heat","wet")
+  
+  single_totals <- data.table(
+    set      = haz_cols,
+    exposure = sapply(haz_cols, \(s) q1_dat[get(s) == TRUE, sum(value, na.rm = TRUE)])
+  )[order(set)]  
+  
+  # Need to ensure order of bars is the same as the upset plot, it works in this example but needs further testing
+  
+    p_set<-ggplot(single_totals,
+                   aes(x = exposure,
+                       y = factor(set, levels = rev(single_totals$set)))) +
+    geom_col(width = .55, fill = "grey40") +
+    geom_text(aes(label = comma(exposure)),
+              hjust = 1.05, colour = "white", size = 3) +
+    scale_x_reverse(position = "top",expand = expansion(mult = c(0, 0))) +  # ← reverse!
+    coord_cartesian(clip = "off") +
+    theme(
+      axis.text.y        = element_blank(),
+      axis.ticks.y       = element_blank(),
+      axis.title = element_blank(),
+      panel.grid = element_blank(),
+      panel.background = element_blank(),
+      plot.margin        = margin(t = 0, r = 0, b = 0, l = 0)  
+    )
+    
+    # Assemble plots using patchwork
+    design <- "
+A
+B
+"  
+    #  upset panel 
+    plot_right <-wrap_plots(
+        A =p_up,   # empty top-left cell
+        B = patchwork::plot_spacer(), 
+        design  = design,
+        heights = c(0.9999, 0.0001)      
+      )
+    
+    # total bars   
+    plot_left <-wrap_plots(
+      A =patchwork::plot_spacer(),  
+      B = p_set, 
+      design  = design,
+      heights = c(0.93, 0.07)      
+    )
+
+    # merged plot
+    final_plot <-
+      (plot_left | plot_right) +         
+      plot_layout(widths = c(3, 7))  
+    
+    return(final_plot)
+  }
+  s1_lab<-haz_merge$scenario1[1]
+  s2_lab<-haz_merge$scenario2[1]
+  diff_lab<-paste0("Δ ",haz_merge$scenario2[1]," minus ",plot_title=haz_merge$scenario1[1])
+  
+  up_s1<-up_plot(data=q1_dat,val_col = "value1",exp_lab=user_selections$unit,plot_title=s1_lab) 
+  up_s2<-up_plot(data=q1_dat,val_col = "value2",exp_lab=user_selections$unit,plot_title=s2_lab)
+  # An issue with difference is that the unit changes from B to M, we might want to set an override where the unit selection can be derived from
+  # a different column
+  up_diff<-up_plot(data=q1_dat,val_col = "diff",exp_lab=user_selections$unit,plot_title=diff_lab)
+  
+  up_s1p<-up_plot(data=q1_dat,val_col = "perc1",exp_lab="%",plot_title=s1_lab) 
+  up_s2p<-up_plot(data=q1_dat,val_col = "perc2",exp_lab="%",plot_title=s2_lab) 
+  up_diffp<-up_plot(data=q1_dat,val_col = "perc_diff",exp_lab="%",plot_title=diff_lab) 
+  
+      
+  # Q2: Crops>Hazards ####
+
+  q2_fun<-function(plot_dat,
+                   exp_lab,
+                   n_y.groups=NULL,
+                   y_axis = "crop",
+                   val_col,
+                   haz_levels,
+                   cols_fill,
+                   plot_title,
+                   is_compound=T,
+                   plotly=T){
+
+  plot_dat[,value:=plot_dat[[val_col]]]
+  plot_dat[,y_axis:=plot_dat[[y_axis]]]
+
+  if(grepl("diff",val_col)){
+    y_order <- plot_dat[hazard!="no hazard" & value>0, .(tot = sum(value)), by = y_axis][
+    order(-tot), y_axis]
+  }else{
+    y_order <- plot_dat[hazard!="no hazard" , .(tot = sum(value)), by = y_axis][
+      order(-tot), y_axis]
+  }
+  
+  if(!is.null(n_y.groups)){
+    y_order<-y_order[1:n_y.groups]
+    plot_dat<-plot_dat[y_axis %in% y_order]
+  }
+  
+  plot_dat[ , y_axis := factor(y_axis, levels = rev(yorder))]
+  plot_dat[ , hazardf := factor(hazard, levels = haz_levels)]
+  
+  result<-ggplot(plot_dat,
+                 aes(x = value,
+                     y = y_axis,
+                     fill = hazardf, 
+                     text = paste0("Y-group: ", y_axis, "<br>",
+                                   "Hazard: ", hazard, "<br>",
+                                   "Exposure: ", value))) +             
+    scale_x_continuous(labels = label_number(scale_cut = cut_short_scale())) +
+    scale_fill_manual(values = cols_fill,
+                      breaks  = haz_levels,     # keep the legend in that order
+                      drop    = FALSE) +  
+    labs(x = lab,
+         y = NULL,
+         title = plot_title,
+         fill = "Hazard") +                 # legend title
+    theme_minimal(base_size = 12) +
+    theme(panel.grid.major.y = element_blank())
+  
+  if(is_compound==T){
+    result<-result+geom_bar(stat="identity",colour="grey20",size=0.2)
+  }else{
+    result<-result+geom_col(position = position_dodge(width = 0.9)) 
+  }
+  
+  if(plotly){
+    result<-ggplotly(result,tooltip = "text")
+  }
+  return(result)
+  
+  }
+  
+  # n_crops to show
+  n_crops<-15
+  if(n_crops>length(user_selections$crop)){
+    n_crops<-length(user_selections$crop)
+  }
+  
+  q2_s1<-q2_fun(plot_dat=haz_merge[admin0_name=="all" & crop!="all" & hazard %in% compound_set_full],
+         exp_lab = user_selections$unit,
+         n_y.groups=n_crops,
+         val_col="value1",
+         haz_levels = compound_set_full,
+         cols_fill = cols_fill,
+         plot_title=haz_merge$scenario1[1])
+  
+  q2_s2<-q2_fun(plot_dat=haz_merge[admin0_name=="all" & crop!="all" & hazard %in% compound_set_full],
+                exp_lab = user_selections$unit,
+                n_y.groups=n_crops,
+                val_col="value2",
+                haz_levels = compound_set_full,
+                cols_fill = cols_fill,
+                plot_title=haz_merge$scenario2[1])
+  
+  q2_diff<-q2_fun(plot_dat=haz_merge[admin0_name=="all" & crop!="all" & hazard %in% compound_set_full],
+                exp_lab = user_selections$unit,
+                n_y.groups=n_crops,
+                val_col="diff",
+                haz_levels = compound_set_full,
+                cols_fill = cols_fill,
+                plot_title=haz_merge$scenario2[1])
+  
+  q2_s1p<-q2_fun(plot_dat=haz_merge[admin0_name=="all" & crop!="all" & hazard %in% compound_set_full],
+                palette="Set 1",
+                exp_lab = "%",
+                n_crops=n_crops,
+                val_col="perc1",
+                haz_levels = compound_set_full,
+                cols_fill = cols_fill,
+                plot_title=haz_merge$scenario1[1])
+  
+  q2_s2p<-q2_fun(plot_dat=haz_merge[admin0_name=="all" & crop!="all" & hazard %in% compound_set_full],
+                exp_lab = user_selections$unit,
+                n_y.groups=n_crops,
+                val_col="perc2",
+                haz_levels = compound_set_full,
+                cols_fill = cols_fill,
+                plot_title=haz_merge$scenario2[1])
+  
+  q2_diffp<-q2_fun(plot_dat=haz_merge[admin0_name=="all" & crop!="all" & hazard %in% compound_set_full],
+                  exp_lab = user_selections$unit,
+                  n_y.groups=n_crops,
+                  val_col="perc_diff",
+                  haz_levels = compound_set_full,
+                  cols_fill = cols_fill,
+                  plot_title=haz_merge$scenario2[1])
+  
+  # Simple versions ofcompound plot
+  q2_s1<-q2_fun(plot_dat=haz_merge[admin0_name=="all" & crop!="all" & hazard %in% compound_set_simple],
+                exp_lab = user_selections$unit,
+                n_y.groups=n_crops,
+                val_col="value1",
+                haz_levels = compound_set_simple,
+                cols_fill = cols_fill_simple,
+                plot_title=haz_merge$scenario1[1])
+  
+  # Non compound plot
+  q2_s1nc<-q2_fun(plot_dat=haz_merge[admin0_name=="all" & crop!="all" & hazard %in% solo_set],
+                exp_lab = user_selections$unit,
+                n_y.groups=n_crops,
+                val_col="value1",
+                is_compound=F,
+                haz_levels = solo_set,
+                cols_fill = cols_fill_solo,
+                plot_title=haz_merge$scenario1[1])
   
   # Q3: Hazards>Crops
   
